c72371ead9d29ff369a5fef36c851f27b399a6a4
Anugrah-Chandran <anugrahchandran2028@cs.sjcetpalai.ac.in>
2024-10-18 15:57:13 +0530

    Update PythonOperators.py

--- PythonOperators.py
+++ PythonOperators.py
@@ -1,3 +1,8 @@
+'''
+Author: Anugrah Chandran
+Date: 18-10-24
+Description : Python program that demonstrates the usage of arithmetic, comparison, and logical operators. Perform a few operations and print the results.
+'''
 number1 = int(input("Enter the first number:"))
 number2 = int(input("Enter the second number:"))
 print("sum=",number1+number2,", Division=",number1/number2)
